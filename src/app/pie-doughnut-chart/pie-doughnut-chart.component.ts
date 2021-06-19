aecb2e54f676c2727389e544c3c377a92c853b75
Ervis Trupja <ervistrupja@outlook.com>
2021-06-19 15:09:54 +0200

    added pie and doughnut chart

--- src/app/pie-doughnut-chart/pie-doughnut-chart.component.ts
+++ src/app/pie-doughnut-chart/pie-doughnut-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Label, SingleDataSet } from 'ng2-charts';
 
 @Component({
   selector: 'app-pie-doughnut-chart',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PieDoughnutChartComponent implements OnInit {
 
+  chartType:ChartType = "pie";
+  chartLabels: Label[] = ['YES','NO', 'N/A']
+  chartData: SingleDataSet = [80, 15, 5];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  switchChartType(event: any){
+    if(this.chartType == 'pie'){
+      this.chartType = 'doughnut';
+    } else {
+      this.chartType = 'pie';
+    }
+  }
+  
 }
