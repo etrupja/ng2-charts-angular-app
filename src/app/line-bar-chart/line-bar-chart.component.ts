1c1b0c1732e3033ad16a8722dea9e21601aefd5e
Ervis Trupja <ervistrupja@outlook.com>
2021-06-19 14:41:06 +0200

    added line chart

--- src/app/line-bar-chart/line-bar-chart.component.ts
+++ src/app/line-bar-chart/line-bar-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-line-bar-chart',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LineBarChartComponent implements OnInit {
 
+  chartType:ChartType = "line";
+  chartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  chartData: ChartDataSets[] = [
+    { data: [85, 72, 78, 75, 77, 75, 85, 72, 78, 75, 88, 90], label: 'YES' },
+    { data: [15, 28, 22, 25, 23, 25, 15, 28, 22, 25, 12, 10], label: 'NO' }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }
