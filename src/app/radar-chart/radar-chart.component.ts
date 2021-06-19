5ea398286298a9c2533cded532ca97c9ef78e0d8
Ervis Trupja <ervistrupja@outlook.com>
2021-06-19 15:18:49 +0200

    added radar chart

--- src/app/radar-chart/radar-chart.component.ts
+++ src/app/radar-chart/radar-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-radar-chart',
@@ -6,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./radar-chart.component.css']
 })
 export class RadarChartComponent implements OnInit {
+
+  chartType:ChartType = "radar";
+  chartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  chartData: ChartDataSets[] = [
+    { data: [85, 72, 78, 75, 77, 75, 85, 72, 78, 75, 88, 90], label: 'YES' },
+    { data: [15, 28, 22, 25, 23, 25, 15, 28, 22, 25, 12, 10], label: 'NO' }
+  ]
 
   constructor() { }
 
