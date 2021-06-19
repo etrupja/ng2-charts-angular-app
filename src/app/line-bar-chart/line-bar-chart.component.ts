2129262e72d0ce497b32ff87f196666a77d5ec96
Ervis Trupja <ervistrupja@outlook.com>
2021-06-19 17:14:02 +0200

    added hover and click events

--- src/app/line-bar-chart/line-bar-chart.component.ts
+++ src/app/line-bar-chart/line-bar-chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartDataSets, ChartType } from 'chart.js';
-import { Label } from 'ng2-charts';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-line-bar-chart',
@@ -14,6 +14,14 @@ export class LineBarChartComponent implements OnInit {
   chartData: ChartDataSets[] = [
     { data: [85, 72, 78, 75, 77, 75, 85, 72, 78, 75, 88, 90], label: 'YES' },
     { data: [15, 28, 22, 25, 23, 25, 15, 28, 22, 25, 12, 10], label: 'NO' }
+  ];
+  chartColors: Color[] = [
+    {
+      backgroundColor:  'rgb(183, 252, 182, 0.5)'
+    },
+    {
+      backgroundColor: 'rgb(245, 76, 76, 0.8)'
+    }
   ]
 
   constructor() { }
@@ -27,6 +35,25 @@ export class LineBarChartComponent implements OnInit {
     } else {
       this.chartType = 'bar';
     }
+  }
+
+  onChartClick(event: any){
+    if(event.active.length > 0){
+      const chart = event.active[0]._chart;
+      const activePoints = chart.getElementAtEvent(event.event);
+      const clickedElementIndex = activePoints[0]._index;
+
+      const label = chart.data.labels[clickedElementIndex];
+      const value1 = chart.data.datasets[0].data[clickedElementIndex];
+      const value2 = chart.data.datasets[1].data[clickedElementIndex];
+
+      console.log(clickedElementIndex, label, value1);
+      console.log(clickedElementIndex, label, value2);
+    }
+  }
+
+  onChartHover(event: any){
+    this.chartColors[0].borderColor = "red";
   }
   
 
